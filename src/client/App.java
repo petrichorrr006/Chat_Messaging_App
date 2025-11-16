fc6e082253a34817a41ea4e6666065d7a8f47107
ballm <148713797+zhalisher@users.noreply.github.com>
2025-11-16 15:25:11 +0500

    Add files via upload

--- src/client/App.java
+++ src/client/App.java
@@ -9,16 +9,20 @@ import factory.Factory;
 import observer.*;
 import observer.concrete_subscriber.ConsoleListener;
 import product.component.MessageComponent;
+import strategy.EmailStrategy;
+import strategy.SendStrategy;
+import strategy.SmsStrategy;
 
 
 public class App {
     public static void main(String[] args) {
+        // observer + adapter
         MessageBroker messageBroker = new MessageBroker();
         messageBroker.addListener(new ConsoleListener());
-
         messageBroker.addListener(new ExternalServiceAdapter(new XmppService()));
         messageBroker.addListener(new ExternalServiceAdapter(new RestService()));
 
+        // builder + factory
         Builder builder = Factory.createBuilder("Text");
 
         MessageComponent message = builder
@@ -26,10 +30,15 @@ public class App {
                 .setRecipient("Alexander")
                 .setContent("some random text lorem lorem lorem")
                 .build();
+        // decorator
         message = new EncryptionDecorator(message);
         message = new LoggingDecorator(message);
 
-
+        // strategy
+        SendStrategy strategy = new EmailStrategy();
+        SendStrategy strategy2 = new SmsStrategy();
+        strategy.send(message);
+        strategy2.send(message);
         messageBroker.publish(message);
     }
 }
