ee73ee470e8d85be60dffa10c3e660f31b267249
ballm <148713797+zhalisher@users.noreply.github.com>
2025-11-16 15:18:48 +0500

    Add files via upload

---
+++ src/strategy/EmailStrategy.java
@@ -0,0 +1,10 @@
+package strategy;
+
+import product.component.MessageComponent;
+
+public class EmailStrategy implements SendStrategy {
+    @Override
+    public void send(MessageComponent message) {
+        System.out.println("[EMAIL] Sending to " + message.getRecipient() + ": " + message.getContent());
+    }
+}
